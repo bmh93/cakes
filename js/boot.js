8ace9befa9cc3f197ed8b2d59a7b1d7401c10223
bmh93 <bmh3892@gmail.com>
2021-10-11 13:57:07 +0300

    upload

---
+++ js/boot.js
@@ -0,0 +1,14 @@
+/*$(function () {
+	'use strict';
+
+	var winH = $(window).height(),
+		navH= $('.navbar').innerHeight();
+	$('.slider, .carousel-item').height(winH - navH );
+});*/
+
+/* fittext plugin */
+$(function () {
+	'use strict';
+	 $(".fit").fitText();
+
+	});
